d0c68ff739bb0a1e27a888c42ed9b2694fdc1426
liuyuxuan123 <1010134430@qq.com>
2018-11-14 01:00:29 -0500

    Circle Progress Bar

--- CircleProgressBar/ViewController.swift
+++ CircleProgressBar/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.strokeEnd = 0
+        shapeLayer.lineWidth = 10
+        self.view.layer.addSublayer(shapeLayer)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    @objc func handleTap(){
+        print("Attempt to animate")
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2.0
+        shapeLayer.add(basicAnimation, forKey: "basic")
     }
 
 
