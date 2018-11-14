a31662c4af988590e61c8dc90b35741d58cd422c
liuyuxuan123 <1010134430@qq.com>
2018-11-14 01:08:40 -0500

    Stroke

--- CircleProgressBar/ViewController.swift
+++ CircleProgressBar/ViewController.swift
@@ -23,8 +23,17 @@ class ViewController: UIViewController {
         shapeLayer.path = circularPath.cgPath
         shapeLayer.strokeColor = UIColor.red.cgColor
         shapeLayer.strokeEnd = 0
-        shapeLayer.lineWidth = 10
+        shapeLayer.lineWidth = 20
         self.view.layer.addSublayer(shapeLayer)
+        
+        
+        let circularPath2 = UIBezierPath(arcCenter: CGPoint(x: self.view.bounds.size.width / 2, y: 100), radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let alayer = CAShapeLayer()
+        alayer.path = circularPath2.cgPath
+
+        alayer.strokeColor = UIColor.red.cgColor
+        self.view.layer.addSublayer(alayer)
+        
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
